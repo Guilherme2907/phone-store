e7c5168ce47d7865fd0802209f5888e62b269668
Guilherme2907 <guilherme.magalhaes2907@gmail.com>
2019-09-20 14:47:39 -0300

    Criando os componentes e configurando as rotas

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import React from "react";
-// import logo from "./logo.svg";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-
-
+import { Route,Switch } from "react-router-dom";
+import NavBar from "./components/NavBar";
+import ProductList from "./components/ProductList";
+import Details from './components/Details';
+import Cart from './components/Cart';
+import Default from './components/Default';
 
 function App() {
   return (
   <React.Fragment>
-      <h3>Hello world</h3>
+      <NavBar />
+
+      <Switch>
+          <Route path='/' exact component={ProductList} />
+          <Route path='/details'  component={Details} />
+          <Route path='/cart'  component={Cart} />
+          <Route component={Default} />
+      </Switch>
   </React.Fragment>
   );
 }
