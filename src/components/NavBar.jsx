8bfbb6bfe9677d6b40afc490aae9102029f6965f
Guilherme2907 <guilherme.magalhaes2907@gmail.com>
2019-09-21 11:50:28 -0300

    Estilizando o navbar e criando o componente para estilização do botão

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,11 +1,37 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom';
+// import styled from 'styled-components';
+
+import logo from '../logo.svg';
+import { ButtonContainer } from './Button';
 
 export default class NavBar extends Component {
     render() {
         return (
-            <div>
-                <h3>NavBar</h3>
-            </div>
+           <nav className="navbar navbar-expand-sm bg-primary navbar-dark px-sm-5">
+               <Link to='/'>
+                <img src={logo} alt="logo" className='navbar-brand'/>
+               </Link>
+
+               <ul className="navbar-nav align-items-center">
+                   <li className="nav-item ml-5">
+                       <Link to='/'>
+                           Products
+                       </Link>
+                   </li>
+               </ul>
+
+               <Link to='/cart' className='ml-auto'>
+                   <ButtonContainer>
+                      <span className='mr-2'>
+                      <i className="fas fa-cart-plus"></i>
+                      </span>
+                       My Cart
+                   </ButtonContainer>
+               </Link>
+           </nav>
         )
     }
 }
+
+
